4bd49b992d17ed70f79738735f65bc128ed1e42c
valentin-ortiz <vortiz@capi.lat>
2022-12-09 01:56:35 -0600

    Actualización

---
+++ app/Models/dataAnality/SoftwareDevelopersLocation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\dataAnality;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SoftwareDevelopersLocation extends Model
+{
+    use HasFactory;
+}
